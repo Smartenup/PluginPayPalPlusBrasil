f8ac38f16fcc44094845edc3ab1f794d79ed7562
Pedro Ivan Usinskas <uiskas2@gmail.com>
2018-09-28 09:21:37 -0300

    Inicio

---
+++ Nop.Plugin.Payments.PayPalPlusBrasil/Models/Message/Response/TokenResponse.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Nop.Plugin.Payments.PayPalPlusBrasil.Models.Message.Response
+{
+    public class TokenResponse
+    {
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        [JsonProperty("nonce")]
+        public string Nonce { get; set; }
+
+        [JsonProperty("access_token")]
+        public string AcessToken { get; set; }
+
+        [JsonProperty("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonProperty("app_id")]
+        public string AppID { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpeiredIn { get; set; }
+    }
+}
---
+++ Nop.Plugin.Payments.PayPalPlusBrasil/RouteProvider.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Nop.Web.Framework.Mvc.Routes;
+
+namespace Nop.Plugin.Payments.PayPalPlusBrasil
+{
+    public partial class RouteProvider : IRouteProvider
+    {
+        public void RegisterRoutes(RouteCollection routes)
+        {
+            //IPN
+            routes.MapRoute("Plugin.Payments.PayPalPlusBrasil.IPNHandler",
+                 "Plugins/PaymentPayPalPlusBrasil/IPNHandler",
+                 new { controller = "PaymentPayPalPlusBrasil", action = "IPNHandler" },
+                 new[] { "Nop.Plugin.Payments.PayPalPlusBrasil.Controllers" }
+            );
+        }
+        public int Priority
+        {
+            get
+            {
+                return 0;
+            }
+        }
+    }
+}
